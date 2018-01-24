d47aba1ed3435327da2b40da34a20830ed487e51
chiranjev <shl12321@rediffmail.com>
2018-01-24 23:38:41 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,56 @@
+function fcheckPass()
+{
+    //Store the password field objects into variables ...
+    var pass1 = document.getElementById('fpswd');
+    var pass2 = document.getElementById('fcpswd');
+    //Store the Confimation Message Object ...
+    var message = document.getElementById('fconfirmMessage');
+    //Set the colors we will be using ...
+    var goodColor = "#66cc66";
+    var badColor = "#ff6666";
+    //Compare the values in the password field
+    //and the confirmation field
+    if(pass1.value == pass2.value){
+        //The passwords match.
+        //Set the color to the good color and inform
+        //the user that they have entered the correct password
+        pass2.style.backgroundColor = goodColor;
+        message.style.color = goodColor;
+        message.innerHTML = "Passwords Match"
+    }else{
+        //The passwords do not match.
+        //Set the color to the bad color and
+        //notify the user.
+        pass2.style.backgroundColor = badColor;
+        message.style.color = badColor;
+        message.innerHTML = "Passwords Do Not Match!"
+    }
+}
+function bcheckPass()
+{
+    //Store the password field objects into variables ...
+    var pass1 = document.getElementById('bpswd');
+    var pass2 = document.getElementById('bcpswd');
+    //Store the Confimation Message Object ...
+    var message = document.getElementById('bconfirmMessage');
+    //Set the colors we will be using ...
+    var goodColor = "#66cc66";
+    var badColor = "#ff6666";
+    //Compare the values in the password field
+    //and the confirmation field
+    if(pass1.value == pass2.value){
+        //The passwords match.
+        //Set the color to the good color and inform
+        //the user that they have entered the correct password
+        pass2.style.backgroundColor = goodColor;
+        message.style.color = goodColor;
+        message.innerHTML = "Passwords Match"
+    }else{
+        //The passwords do not match.
+        //Set the color to the bad color and
+        //notify the user.
+        pass2.style.backgroundColor = badColor;
+        message.style.color = badColor;
+        message.innerHTML = "Passwords Do Not Match!"
+    }
+}
